baa189e454079cf3523040066e816253b4fddf2b
Sascha Brawer <sascha@brawer.ch>
2015-06-16 18:19:11 +0200

    [CLDR] Improve phonemic transcription for Armenian

--- cldr/check_translit_hy.py
+++ cldr/check_translit_hy.py
@@ -7,17 +7,57 @@ rules = codecs.open('hy-hy_FONIPA.txt', 'r', 'utf-8').read()
 translit = icu.Transliterator.createFromRules(
     "hy-hy_FONIPA", rules, icu.UTransDirection.FORWARD)
 
-words = {}
-udhr = codecs.open('udhr-hy.txt', 'r', 'utf-8').read()
-for c in '֊-։«,:;.0123456789()': udhr = udhr.replace(c, ' ')
+def makePhonemeSet(s):
+    pat = []
+    for phoneme in s.split():
+        if len(phoneme) == 1:
+            pat.append(phoneme)
+        else:
+            pat.append('{%s}' % phoneme)
+    #print ' '.join(pat).encode('utf-8')
+    result = icu.UnicodeSet()
+    result.applyPattern('[%s]' % ' '.join(pat))
+    return result
 
+ARMENIAN_GRAPHEMES = icu.UnicodeSet()
+ARMENIAN_GRAPHEMES.applyPattern('[:Armn:]')
 
+ARMENIAN_PHONEMES = makePhonemeSet("""
 
+    m n
+    p pʰ t tʰ k kʰ b d g 
+    t͡s t͡sʰ t͡ʃ t͡ʃʰ d͡z d͡ʒ
+    f v s z ʃ ʒ x ɣ h
+    l j r ɾ
 
-for word in udhr.split():
-    words[word] = words.get(word, 0) + 1
-for count, word in reversed(sorted([(v,k) for k,v in words.items()])):
-    t = translit.transliterate(word)
-    print ('%s\t%s' % (word, t)).encode('utf-8')
+    i u
+    ɛ ə o
+    a
 
+""")
 
+def match(s, unicodeset):
+    return icu.UnicodeSet.span(
+        unicodeset, s, icu.USetSpanCondition.SPAN_CONTAINED) == len(s)
+
+def check(testdatapath):
+    num_lines = 0
+    for line in codecs.open(testdatapath, 'r', 'utf-8'):
+        num_lines += 1
+        line = line.strip()
+        if not line or line[0] == '#':
+            continue
+        graph, expected_phon = line.split('\t')
+        if not match(graph, ARMENIAN_GRAPHEMES):
+            print(('%s:%d: Non-Armenian graphemes in "%s"' %
+                   (testdatapath, num_lines, graph)).encode('utf-8'))
+        if not match(expected_phon, ARMENIAN_PHONEMES):
+            print(('%s:%d: Non-Armenian phonemes in "%s"' %
+                   (testdatapath, num_lines, expected_phon)).encode('utf-8'))
+        actual_phon = translit.transliterate(graph)
+        if expected_phon != actual_phon:
+            print(('%s:%d: Expected %s → %s, got %s' %
+                   (testdatapath, num_lines,
+                    graph, expected_phon, actual_phon)).encode('utf-8'))
+
+check('test-hy-hy_FONIPA.txt')
