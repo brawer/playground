bb5a66b16bad3593875d97c531571f580db35a81
Sascha Brawer <sascha@brawer.ch>
2015-06-25 15:57:10 +0200

    Extract utility functions into common library
    This will allow re-using them across languages.

--- cldr/check_translit_my.py
+++ cldr/check_translit_my.py
@@ -3,20 +3,10 @@ from __future__ import unicode_literals
 import codecs
 import icu
 
-BURMESE_GRAPHEMES = icu.UnicodeSet()
-BURMESE_GRAPHEMES.applyPattern('[:Mymr:]')
+from cldr_util import makePhonemeSet, match, check, regtest
 
-def makePhonemeSet(s):
-    pat = []
-    for phoneme in s.split():
-        if len(phoneme) == 1:
-            pat.append(phoneme)
-        else:
-            pat.append('{%s}' % phoneme)
-    print ' '.join(pat).encode('utf-8')
-    result = icu.UnicodeSet()
-    result.applyPattern('[%s]' % ' '.join(pat))
-    return result
+GRAPHEMES = icu.UnicodeSet()
+GRAPHEMES.applyPattern('[:Mymr:]')
 
 # TODO: Differences to JIPA, which we should resolve:
 # - in JIPA, /ɴ/ is not in the phoneme list
@@ -33,7 +23,7 @@ def makePhonemeSet(s):
 #
 # TODO: Can/should we emit syllable markers? /./
 
-BURMESE_PHONEMES = makePhonemeSet("""
+PHONEMES = makePhonemeSet("""
 
     m̥ m n̥ n ɲ̥ ɲ ŋ̊  ŋ ɴ
     p pʰ b t tʰ d t͡ɕ t͡ɕʰ d͡ʑ k kʰ g ʔ
@@ -60,53 +50,6 @@ BURMESE_PHONEMES = makePhonemeSet("""
 
 """)
 
-def match(s, unicodeset):
-    return icu.UnicodeSet.span(
-        unicodeset, s, icu.USetSpanCondition.SPAN_CONTAINED) == len(s)
 
-def check(path):
-    prefixes = {}
-    num_lines = 0
-    for line in codecs.open(path, 'r', 'utf-8'):
-        num_lines += 1
-        line = line.strip()
-        if not line or line[0] == '#':
-            continue
-        assert line[-1] == ';'
-        graph, arrow, phon = line[:-1].split()
-        assert arrow == '→'
-        if graph[:-1] in prefixes:
-            error = ('%s:%d: %s hidden by %s, defined on line %d' %
-                     (path, num_lines, graph, graph[:-1], prefixes[graph[:-1]]))
-            print(error.encode('utf-8'))
-        else:
-            prefixes[graph] = num_lines
-        if not match(graph, BURMESE_GRAPHEMES):
-            print(('%s:%d: Non-Burmese graphemes in "%s"' %
-                  (path, num_lines, line)).encode('utf-8'))
-        if not match(phon, BURMESE_PHONEMES):
-            print(('%s:%d: Non-Burmese phonemes in "%s"' %
-                   (path, num_lines, line)).encode('utf-8'))
-
-def regtest(translit_name):
-    rules = codecs.open('%s.txt' % translit_name, 'r', 'utf-8').read()
-    translit = icu.Transliterator.createFromRules(
-        translit_name, rules, icu.UTransDirection.FORWARD)
-    num_lines = 0
-    path = 'test-%s.txt' % translit_name
-    for line in codecs.open(path, 'r', 'utf-8'):
-        num_lines += 1
-        graph, expected_ipa = line.strip().split('\t')
-        if not match(graph, BURMESE_GRAPHEMES):
-            print(('%s:%d: Non-Burmese graphemes in "%s"' %
-                  (path, num_lines, graph)).encode('utf-8'))
-        if not match(expected_ipa, BURMESE_PHONEMES):
-            print(('%s:%d: Non-Burmese phonemes in "%s"' %
-                   (path, num_lines, expected_ipa)).encode('utf-8'))
-        actual_ipa = translit.transliterate(graph)
-        if actual_ipa != expected_ipa:
-            print(('%s:%d: Expected "%s" but got "%s" for "%s"' %
-                   (path, num_lines, expected_ipa, actual_ipa, graph)).encode('utf-8'))
-
-check('my-my_FONIPA.txt')
-regtest('my-my_FONIPA')
+check('my-my_FONIPA.txt', GRAPHEMES, PHONEMES)
+regtest('my-my_FONIPA', GRAPHEMES, PHONEMES)
---
+++ cldr/cldr_util.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+import codecs
+import icu
+
+
+def makePhonemeSet(s):
+    pat = []
+    for phoneme in s.split():
+        if len(phoneme) == 1:
+            pat.append(phoneme)
+        else:
+            pat.append('{%s}' % phoneme)
+    print ' '.join(pat).encode('utf-8')
+    result = icu.UnicodeSet()
+    result.applyPattern('[%s]' % ' '.join(pat))
+    return result
+
+
+def match(s, unicodeset):
+    return icu.UnicodeSet.span(
+        unicodeset, s, icu.USetSpanCondition.SPAN_CONTAINED) == len(s)
+
+
+def check(path, graphemes, phonemes):
+    prefixes = {}
+    num_lines = 0
+    for line in codecs.open(path, 'r', 'utf-8'):
+        num_lines += 1
+        line = line.strip()
+        if not line or line[0] == '#':
+            continue
+        assert line[-1] == ';'
+        graph, arrow, phon = line[:-1].split()
+        assert arrow == '→'
+        if graph[:-1] in prefixes:
+            error = ('%s:%d: %s hidden by %s, defined on line %d' %
+                     (path, num_lines, graph, graph[:-1], prefixes[graph[:-1]]))
+            print(error.encode('utf-8'))
+        else:
+            prefixes[graph] = num_lines
+        if not match(graph, graphemes):
+            print(('%s:%d: Unexpected graphemes in "%s"' %
+                  (path, num_lines, line)).encode('utf-8'))
+        if not match(phon, phonemes):
+            print(('%s:%d: Unexpected phonemes in "%s"' %
+                   (path, num_lines, line)).encode('utf-8'))
+
+            
+def regtest(translit_name, graphemes, phonemes):
+    rules = codecs.open('%s.txt' % translit_name, 'r', 'utf-8').read()
+    translit = icu.Transliterator.createFromRules(
+        translit_name, rules, icu.UTransDirection.FORWARD)
+    num_lines = 0
+    path = 'test-%s.txt' % translit_name
+    for line in codecs.open(path, 'r', 'utf-8'):
+        num_lines += 1
+        graph, expected_ipa = line.strip().split('\t')
+        if not match(graph, graphemes):
+            print(('%s:%d: Unexpected graphemes in "%s"' %
+                  (path, num_lines, graph)).encode('utf-8'))
+        if not match(expected_ipa, phonemes):
+            print(('%s:%d: Unexpected phonemes in "%s"' %
+                   (path, num_lines, expected_ipa)).encode('utf-8'))
+        actual_ipa = translit.transliterate(graph)
+        if actual_ipa != expected_ipa:
+            print(('%s:%d: Expected "%s" but got "%s" for "%s"' %
+                   (path, num_lines, expected_ipa, actual_ipa, graph)).encode('utf-8'))
