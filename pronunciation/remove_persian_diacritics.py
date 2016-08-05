a0ad270179f3323af422c6f61778f057b0d9e0aa
Sascha Brawer <sascha@brawer.ch>
2016-08-05 14:34:28 +0200

    Add an ad-hoc script for removing diacritics from a Persian corpus

---
+++ pronunciation/remove_persian_diacritics.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python2.7
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+import codecs, os, urllib
+
+
+"""Removes diacritics from a Persian corpus. Output is tab-separated UTF-8."""
+
+
+def unaccent(word):
+    for c in u'\u064B\u064C\u064D\u064E\u064F\u0650\u0651\u0652\u0653':
+        word = word.replace(c, '')
+    return word
+
+
+def load_corpus():
+    path = '/tmp/updt-with-diacritics.txt'
+    if not os.path.exists(path):
+        url = ('https://github.com/googlei18n/language-resources/raw/master/'
+               'third_party/fa/updt/updt-with-diacritics.txt')
+        with open(path, 'w') as out:
+            out.write(urllib.urlopen(url).read())
+    return path
+
+
+def unaccent_corpus(path):
+    for line in codecs.open(path, 'r', 'utf-8'):
+        for c in ' ؛.,!?[+/];»:»()“‘" ؟؉؊؍،–—': line = line.replace(c, ' ')
+        words = line.split()
+        for word in words:
+            # Remove word-final U+0652 Arabic Sukun
+            if word.endswith('\u0652'):
+                word = word[:-1]
+            print u'\t'.join([unaccent(word), word]).encode('utf-8')
+
+
+if __name__ == '__main__':
+    unaccent_corpus(load_corpus())
