cc60bc93c231c918aa42816dc3402adf6ed18c8a
Sascha Brawer <sascha@brawer.ch>
2017-06-06 12:25:56 +0200

    Add IPA transcription for Farsi

---
+++ cldr/check_translit_fa.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+import codecs
+import icu
+
+from cldr_util import makePhonemeSet, match, check, regtest
+
+GRAPHEMES = icu.UnicodeSet()
+GRAPHEMES.applyPattern('[[:Arab:] \u064e  ٰ  ْ  ِ  ُ  ٓ]')
+
+PHONEMES = makePhonemeSet("""
+
+    m n
+    p b t d k ɡ ʔ
+    f v s z ʃ ʒ ʁ ɢ h χ
+    t͡ʃ d͡ʒ
+    l ɾ j w
+    iː uː e o æ ɒː
+
+""")
+
+check('fa-fonipa-t-fa', GRAPHEMES, PHONEMES)
+regtest('fa-fonipa-t-fa', GRAPHEMES, PHONEMES)
