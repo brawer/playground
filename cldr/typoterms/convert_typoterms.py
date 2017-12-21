fef206a4b57eb388989c8c0b03a5b9431121bb35
Sascha Brawer <sascha@brawer.ch>
2017-12-21 22:10:36 +0100

    Compare combined style names from CLDR and Windows

--- cldr/typoterms/convert_typoterms.py
+++ cldr/typoterms/convert_typoterms.py
@@ -4,6 +4,19 @@ from __future__ import print_function, unicode_literals
 import codecs, os, unicodedata
 
 PHOTOSHOP_PATH = 'typoterms-photoshop.tsv'
+WINDOWS_PATH = 'typoterms-windows.tsv'
+
+
+def join(parts, lang):
+    """(['Bold', 'Italic'], 'en') --> 'Bold Italic'
+
+    Builds a synthetic face name by joining its parts.
+    `lang` is a BCP47 language code, for example 'en' for English.
+    """
+    if lang in {'th', 'zh', 'zh_Hant'}:
+        return ''.join(parts)
+    return ' '.join(parts)
+
 
 def read_photoshop_typoterms():
     # https://www.microsoft.com/typography/otspec/dvaraxisreg.htm
@@ -15,7 +28,7 @@ def read_photoshop_typoterms():
         'Weight': 'wght',
     }
     path = os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                        'typoterms-photoshop.tsv')
+                        PHOTOSHOP_PATH)
     result = {tag: {'en': name} for name, tag in axis_tags.items()}
     for line in codecs.open(path, 'r', 'utf-8').readlines():
         line = line.strip()
@@ -66,8 +79,9 @@ _WINDOWS_STYLES = {
 
 def read_windows_typoterms():
     path = os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                        'typoterms-windows.tsv')
+                        WINDOWS_PATH)
     styleNames = {}
+    combinedFaceNames = {}
     for line in codecs.open(path, 'r', 'utf-8').readlines():
         line = line.strip()
         if not line or line.startswith('Language\t'):
@@ -83,7 +97,9 @@ def read_windows_typoterms():
             styleNames.setdefault(('slnt', '0', None), {})[lang] = name
         if (category, concept) == ('WWS - Style', 'Unknown'):
             styleNames.setdefault(('slnt', 'unknown', None), {})[lang] = name
-    return {'styleNames': styleNames}
+        if category == 'WWS - combined facename':
+            combinedFaceNames.setdefault(concept, {})[lang] = name
+    return {'styleNames': styleNames, 'combinedFaceNames': combinedFaceNames}
 
 
 def normalize_windows_language(lang):
@@ -164,9 +180,75 @@ def write_xml(terms, lang, out):
     out.write(prefix + '</typographicNames>\n\n')
 
 
+def get_name(names, lang):
+    if lang in names:
+        return names[lang]
+    if '_' in lang:
+        baselang = lang.split('_')[0]
+        if baselang in names:
+            return names[baselang]
+    return names['en']
+
+
+def synthesize_facename(terms, lang, var):
+    styleNames = terms['styleNames']
+    parts = []
+    width = var.get('wdth', '100')
+    if width != '100':
+        widthKey = ('wdth', width, None)
+        parts.append(get_name(styleNames[widthKey], lang))
+    weight = var.get('wght', '400')
+    if weight != '400':
+        weightKey = ('wght', weight, None)
+        parts.append(get_name(styleNames[weightKey], lang))
+    italic = var.get('ital', '0')
+    if italic != '0':
+        italicKey = ('ital', italic, None)
+        parts.append(get_name(styleNames[italicKey], lang))
+    slant = var.get('slnt', '0')
+    if slant != '0':
+        slantKey = ('slnt', slant, None)
+        parts.append(get_name(styleNames[slantKey], lang))
+    if parts:
+        return join(parts, lang)
+    else:
+        return styleNames[('wdth', 100, None)]
+
+
+# Make sure that our heuristic for synthesizing face names gives the same
+# results as in Microsoft's localization data.
+def check_combined_facenames(terms, out):
+    digits = '0123456789'
+    names = get_style_names(terms, 'en')
+    weights = {name: value for (axis, value, alt), name in names.items()
+               if axis == 'wght' and value[0] in digits}
+    widths = {name: value for (axis, value, alt), name in names.items()
+              if axis == 'wdth' and value[0] in digits}
+    combined = {}
+    for widthName, width in widths.items():
+        for weightName, weight in weights.items():
+            parts = []
+            if widthName != 'Normal': parts.append(widthName)
+            if weightName != 'Regular': parts.append(weightName)
+            combinedName = join(parts, 'en')
+            if combinedName:
+                combined[combinedName] = {'wdth': width, 'wght': weight}
+            combined[join(parts + ['Italic'], 'en')] = {'wdth': width, 'wght': weight, 'ital': '1'}
+            combined[join(parts + ['Oblique'], 'en')] = {'wdth': width, 'wght': weight, 'slnt': '12'}
+    out.write('Face\tLanguage\tCLDR\tWindows\n')
+    for combinedKey, combinedNames in sorted(terms['combinedFaceNames'].items()):
+        var = combined[combinedKey]
+        for lang, windowsName in sorted(combinedNames.items()):
+            synthName = synthesize_facename(terms, lang, var)
+            out.write('\t'.join([combinedKey,lang, synthName, windowsName]))
+            out.write('\n')
+
+
 if __name__ == '__main__':
     terms = read_windows_typoterms()
     terms.update(read_photoshop_typoterms())
+    with codecs.open('combined-facenames.tsv', 'w', 'utf-8') as out:
+        check_combined_facenames(terms, out)
     with codecs.open('typoterms.xml', 'w', 'utf-8') as out:
         out.write('<terms>\n')
         for lang in sorted(find_languages(terms)):
