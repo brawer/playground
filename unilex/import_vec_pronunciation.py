c6f44bcb9275e124f5a6eca34d3b62df6b54ded6
Sascha Brawer <sascha@brawer.ch>
2018-01-25 15:55:39 +0100

    [vec] Verify that output phonemes are from a closed set

--- unilex/import_vec_pronunciation.py
+++ unilex/import_vec_pronunciation.py
@@ -4,13 +4,23 @@ from __future__ import unicode_literals
 import codecs, icu, unicodedata
 
 
+PHONEMES = '''
+m n ɲ ŋ
+p b t d k ɡ h
+f v ɾ s z
+l ʎ j w
+t͡ʃ d͡ʒ d͡z
+i u e e̯ o ɛ ɔ a
+'''
+
 IPA_TRANSLIT_RULES = '''
 # References
 # ----------
 # [1] https://en.wikipedia.org/wiki/Venetian_language#Phonology
 # [2] https://en.wikipedia.org/wiki/Help:IPA/Venetian
 #
-# Output:
+# Output phonemes
+# ---------------
 
 $boundary = [^[:L:][:M:][:N:]];
 $e = [e é è];
@@ -18,6 +28,7 @@ $i = [i í ì];
 $ei = [$e $i];
 $vowel = [a á à $ei o ó ò u ú ù];
 
+[[:P:][:Z:]]+ → ' ';
 ::Lower;
 ::NFC;
 ([abefhijklmoptvw]) → $1;
@@ -28,7 +39,8 @@ c $i [\' ’]? → t͡ʃi;
 [c {ch} k q {qu}] → k;
 é → e;
 è → ɛ;
-gl $ei? → ʎ;
+{g l $ei} $vowel → ʎ;
+g l → ʎ;
 ġ → d͡ʒ;
 g $ei → d͡ʒ;
 gn → ɲ;
@@ -47,6 +59,7 @@ r → ɾ;
 s → s;
 {u} $vowel → w;
 [u ú ù] → u;
+y → j;
 ž → ʒ;
 [ż đ {dh}] → d͡z;
 d → d;
@@ -69,8 +82,26 @@ def make_transliterator():
         'vec-vec_FONIPA', IPA_TRANSLIT_RULES, icu.UTransDirection.FORWARD)
 
 
+def make_phoneme_set(s):
+    pat = [u'\\u0020']
+    for phoneme in s.split():
+        if len(phoneme) == 1:
+            pat.append(phoneme)
+        else:
+            pat.append('{%s}' % phoneme)
+    result = icu.UnicodeSet()
+    result.applyPattern('[%s]' % ' '.join(pat))
+    return result
+
+
+def match(s, unicodeset):
+    return icu.UnicodeSet.span(
+        unicodeset, s, icu.USetSpanCondition.SPAN_CONTAINED) == len(s)
+
+
 if __name__ == '__main__':
     translit = make_transliterator()
+    phonemes = make_phoneme_set(PHONEMES)
     print('Count\tForm\tPronunciation\n')
     print('# SPDX-License-Identifier: Unicode-DFS-2016\n')
     for line in codecs.open('data/vec/frequency.txt', 'r', 'utf-8'):
@@ -78,9 +109,10 @@ if __name__ == '__main__':
         if not line:
             continue
         count, form = line.split('\t')
+        if form in {'ʣ', 'ǌ', 'ʦ'}:
+            continue
         count = int(count)
         form = unicodedata.normalize('NFC', form)
         ipa = translit.transliterate(form)
         print('\t'.join((str(count), form, ipa)).encode('utf-8'))
-
-
+        assert match(ipa, phonemes), ipa.encode('utf-8')
