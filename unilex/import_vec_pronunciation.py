56fbc98714046d8c7649a58f7a36e1a99a1ab96b
Sascha Brawer <sascha@brawer.ch>
2018-01-26 17:21:35 +0100

    [vec] Generate stress by applying Maximum-Onset Principle

--- unilex/import_vec_pronunciation.py
+++ unilex/import_vec_pronunciation.py
@@ -32,15 +32,18 @@ $ipa_plosive = [p b t d k ɡ];
 $ipa_fricative = [f v ɾ s z h];
 $ipa_consonant = [$ipa_nasal $ipa_plosive $ipa_fricative l ʎ];
 $ipa_affricate = [{t͡ʃ} {d͡ʒ} {d͡z}];
+$ipa_vowel = [i u e {e̯} o ɛ ɔ a];
 $onset = [
   j w $ipa_consonant $ipa_affricate
-  {ps} {pɾ} {pɾw} {pl} {pw} {bɾ} {bw}
-  {ts} {tɾ} {tɾw} {tl} {tw} {dɾ} {dw}
-  {kɾ} {kw} {kɾw} {kl} {kw} {ɡɾ} {ɡw}
-  {fɾ} {fw} {fɾw} {vɾ} {vw}
+  {mj} {mw} {nj} {nw}
+  {ps} {pɾ} {pɾw} {pl} {pw} {bɾ} {bɾw} {bl} {bw}
+  {ts} {tɾ} {tɾw} {tl} {tw} {dɾ} {dɾw} {dl} {dw}
+  {kɾ} {kw} {kɾw} {kl} {kw} {ɡɾ} {ɡɾw} {ɡl} {ɡw}
+  {fɾ} {fl} {fw} {fɾw} {vɾ} {vw} {ɾw}
   {zm} {zn} {zɲ} {zl}
   {zb} {zbɾ} {zbw} {zd} {zdɾ} {zdw} {zɡ} {zɡɾ} {zɡw} {zv} {zvɾ} {zɾ} {zd͡ʒ} {zw}
   {sp} {spɾ} {spw} {st} {stɾ} {stw} {sk} {skɾ} {skw} {sf} {sfɾ} {sɾ} {st͡ʃ} {sw}
+  {lw}
 ];
 
 ::Lower;
@@ -89,9 +92,11 @@ d → d;
 eɰ → e;
 iɰ → i;
 ɰ → e̯;
-
 ::NULL;
+
+# Move stress marker before syllable onset: [zɡɾaŋfiɲˈae] → [zɡɾaŋfiˈɲae]
 ($onset) ˈ → ˈ $1;
+::NULL;
 '''
 
 
@@ -119,6 +124,46 @@ def match(s, unicodeset):
         unicodeset, s, icu.USetSpanCondition.SPAN_CONTAINED) == len(s)
 
 
+def _build_split_regexps():
+    onsets = list('''
+        j w
+        m n ɲ ŋ
+        p b t d k ɡ
+        f v ɾ s z h
+        l ʎ  t͡ʃ d͡ʒ d͡z
+        mj mw nj nw
+        ps pɾ pɾw pl pw bɾ bɾw bw bl
+        ts tɾ tɾw tl tw dɾ dɾw dw dl
+        kɾ kw kɾw kl kw ɡɾ ɡɾw ɡw ɡl
+        fɾ fl fw fɾw vɾ vw ɾw
+        zm zn zɲ zl
+        zb zbɾ zbw zd zdɾ zdw zɡ zɡɾ zɡw zv zvɾ zɾ zd͡ʒ zw
+        sp spɾ spw st stɾ stw sk skɾ skw sf sfɾ sɾ st͡ʃ sw
+        lw e̯
+    '''.split())
+    onsets.sort(key=len, reverse=True)
+    vowels = list('i u e o ɛ ɔ a'.split())
+    vowels.sort(key=len, reverse=True)
+    return re.compile('((%s)?(%s))' % ('|'.join(onsets), '|'.join(vowels)))
+
+_onset_vowel = _build_split_regexps()
+
+
+def stress(s):
+    if "ˈ" in s or len(s) == 1:
+        return s
+    syll = _onset_vowel.sub(lambda m: "." + m.group(0), s)
+    if syll[0] != '.':
+        return s
+    syll = [s for s in syll.split('.') if s]
+    if len(syll) == 1:
+        return s
+    pos = -2
+    if syll[-1].endswith('oŋ') or syll[-1].endswith('aɾ'):
+        pos = -1
+    return ''.join(syll[:pos] + ["ˈ"] + syll[pos:])
+
+
 if __name__ == '__main__':
     onsets = collections.Counter()
     translit = make_transliterator()
@@ -132,7 +177,7 @@ if __name__ == '__main__':
         count, form = line.split('\t')
         count = int(count)
         form = unicodedata.normalize('NFC', form)
-        ipa = translit.transliterate(form)
+        ipa = ' '.join(stress(w) for w in translit.transliterate(form).split())
         print('\t'.join((str(count), form, ipa)).encode('utf-8'))
         assert match(ipa, phonemes), ipa.encode('utf-8')
         onset = re.split(r'i|u|e|o|ɛ|ɔ|a', ipa.split()[-1])[0]
