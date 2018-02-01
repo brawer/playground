81f8ce857f3cc6eade095a10bd37f7451952d1d5
Sascha Brawer <sascha@brawer.ch>
2018-02-01 17:56:54 +0100

    [fr] Sort entries in imported French pronunciation dictionary

--- unilex/import_fr_pronunciation.py
+++ unilex/import_fr_pronunciation.py
@@ -1,21 +1,48 @@
 # coding: utf-8
 
-from __future__ import unicode_literals
-import codecs, os, re, unicodedata
+from __future__ import print_function, unicode_literals
+import codecs, icu, os, re, unicodedata, urllib
 import xml.etree.ElementTree as etree
 
 
-_MARYTTS_LEXICON_FR = '../../marytts-lexicon-fr/modules/fr/lexicon'
+LEXICON_URL = ('https://raw.githubusercontent.com/brawer/marytts-lexicon-fr/'
+               'master/modules/fr/lexicon/')
+CACHE_DIR = '/tmp/cache-lexicon-fr'
+LOCALE = icu.Locale('fr')
+COLLATOR = icu.Collator.createInstance(LOCALE)
 
 
-def read_allophones(f):
+def open_lexicon_file(filename):
+    cache = os.path.join(CACHE_DIR, filename)
+    if not os.path.exists(cache):
+        content = urllib.urlopen(LEXICON_URL + filename).read()
+        with open(cache, 'w') as out:
+            out.write(content)
+    return open(cache, 'rb')
+
+
+def read_lexicon():
+    lex = []
+    with open_lexicon_file('fr.txt') as f:
+        for line in f:
+            try:
+                line = line.split('#')[0].strip().decode('utf-8')
+            except UnicodeDecodeError:
+                continue
+            columns = line.split()
+            if len(columns) == 2:
+                graph, phon = [col.strip() for col in columns]
+                lex.append((graph, phon))
+    return lex
+
+
+def read_allophones():
+    with open_lexicon_file('allophones.fr.xml') as f:
+        r = etree.parse(f).getroot()
     mapping = {'-': '.', "'": "ˈ", ",": "ˌ"}
-    r = etree.parse(f).getroot()
     for element in (r.findall('./vowel') + r.findall('./consonant')):
         if 'ph' in element.attrib and 'ipa' in element.attrib:
-            ipa = unicode(element.attrib['ipa'])
-            assert unicodedata.normalize('NFC', ipa.strip()) == ipa
-            mapping[element.attrib['ph']] = ipa
+            mapping[element.attrib['ph']] = element.attrib['ipa']
     return mapping
 
 
@@ -25,22 +52,29 @@ def _build_ipa_regexp(allophones):
     return re.compile('(%s|.)' % r)
 
 
-with open(_MARYTTS_LEXICON_FR + '/allophones.fr.xml', 'rb') as f:
-    _allophones = read_allophones(f)
-    _ipa_regexp = _build_ipa_regexp(_allophones)
-
-
-def ipa(xsampa):
-    return _ipa_regexp.sub(lambda x: _allophones[x.group(1)], xsampa)
+def compare_entry(a, b):
+    a = '\t'.join(a)
+    b = '\t'.join(b)
+    if COLLATOR.greater(a, b):
+        return 1
+    elif COLLATOR.greaterOrEqual(a, b):
+        return 0
+    else:
+        return -1
 
 
 if __name__ == '__main__':
-    for line in codecs.open(_MARYTTS_LEXICON_FR + '/fr.txt', 'rb', 'utf-8'):
-        line = line.strip()
-        if not line or line[0] == '#':
-            continue
-        columns = [c.strip() for c in line.split()]
-        if len(columns) < 2:
-            continue
-        word, sampa = columns[:2]
-        print '\t'.join([word, ipa(sampa)]).encode('utf-8')
+    if not os.path.exists(CACHE_DIR):
+        os.mkdir(CACHE_DIR)
+    allophones = read_allophones()
+    ipa_regexp = _build_ipa_regexp(allophones)
+    def ipa(sampa):
+        return ipa_regexp.sub(lambda x: allophones[x.group(1)], sampa)
+    lex = [(word, ipa(sampa)) for word, sampa in read_lexicon()]
+    lex.sort(cmp=compare_entry)
+    print('\t'.join(['Form', 'Pronunciation']))
+    print()
+    print('# SPDX-License-Identifier: Unicode-DFS-2016')
+    print()
+    for word, pron in lex:
+        print('\t'.join([word, pron]).encode('utf-8'))
